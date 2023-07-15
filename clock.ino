3fd5f19e020a1da859077540e43e06e08986b2f7
Christian Lefebvre <christian_lefebvre@laposte.net>
2023-07-15 22:35:30 +0200

    WIP: trying to fix clock shift

--- clock.ino
+++ clock.ino
@@ -1,3 +1,11 @@
+// TODO :
+// - revoir les registre counters pour Uno et tiny
+// - voir si on arrive à compter précisement des secondes (avec une précision suffisante pour une heure ? une journée ? )
+//   nécessite une fonction "+1" sur un struct time ou "+n", ou passer en epoch+x ?
+// - modifier le code pour n'interroger le ds3231 qu'une fois par heure/jour
+// - en // chercher "filtre passe bande" pour calibrer les condo + mettre une diode en sortie de tout sauf la matrice
+// - en // , il faudra un radiateur au 7508, à moins qu'une autre alim suffise
+
 #if defined(__AVR_ATtiny85__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny25__)
 	#define ARDUINO_TINY
 #elif defined(__AVR_MEGA__)
@@ -35,7 +43,7 @@
     // #define DS_SQW A3 TODO
 
     #ifdef USE_BUTTONS
-        #define BUTTONS_PIN 3 // pin 1
+        #define BUTTONS_PIN 3 // pin 2
     #endif
 
     #define MATRIX_CLK 1 // pin 6
@@ -46,11 +54,15 @@
 
 DS3231 rtc;
 
-byte intensity = 3;
+byte intensity = 0;
 LedMatrix ledMatrix = LedMatrix(12, MATRIX_CLK, MATRIX_CS, MATRIX_DIN, intensity);
 
 // int buttonThreshold[] = { 600, 450, 300 };  // value with 22k / 10k / 10k
+#ifdef ARDUINO_UNO
+int buttonThreshold[] = { 700, 600, 500 };  // value with 56k / 10k / 10k
+#else
 int buttonThreshold[] = { 900, 800, 700 };  // value with 56k / 10k / 10k
+#endif
 #ifdef USE_BUTTONS
 Buttons buttons = Buttons(BUTTONS_PIN, 3, buttonThreshold);
 #endif
@@ -64,7 +76,6 @@ void setup() {
 
     /* DS3231 outputs nothing but continues counting when on battery */
     rtc.setControl(0b00000000 , 0b00000000);
-
 #ifdef ARDUINO_UNO
     // initialize external interrupt on button pin A0 (PCINT8)
 #ifdef USE_BUTTONS
@@ -72,12 +83,13 @@ void setup() {
     PCICR  |= 1 << PCIE1;
 #endif
 
-    // timer 1 : WGM 4 = CTC , prescale 3 = /64
+    // timer 1 : WGM 4 = CTC , prescale 3 = /1024
+    // 16MHz / 1024 = 15625Hz
     TCCR1A = 0;
     TCCR1B = (1 << WGM12) | (1 << CS12) |(1 << CS10);
     TCCR1C = 0;
     TIMSK1 = 1 << OCIE1A;
-    OCR1A = 15625;
+    OCR1A = 15625; // = 1s
 #else // ARDUINO_TINY
 #ifdef USE_BUTTONS
     PCMSK |= (1 << PCINT3);
@@ -87,13 +99,15 @@ void setup() {
 	// set Clock to 1MHz instead of 8
 	cli(); CLKPR=0x80 ; CLKPR=0x03; sei();
 
-    TCCR1 = (1 << CTC1) | 0xF; // reset on OCR1A match | prescale 16384
+    TCCR1 = (1 << CTC1) | 0x7; // reset on OCR1A match | prescale 64
     // to debug, output on OC1A = out 1 : | (1 << PWM1A) | (1 << COM1A0)
     OCR1A = 30; // interrupt before counter reset
-    OCR1C = 61; // -> ~1s
+    OCR1C = 125; // 1/125s
     GTCCR = 0;
     TIMSK = 1 << OCIE1A;
 #endif
+
+    updateTimeFromRtc();
 
 #ifdef HAVE_SERIAL
     Serial.println(F("Setup OK"));
@@ -117,12 +131,12 @@ void displayTime(TimeStruct *time, byte offset, byte maxLen) {
     X = ledMatrix.drawChar(X, '0' + time->seconds / 10);
     X = ledMatrix.drawChar(X, '0' + time->seconds % 10);
 
-#ifdef HAVE_SERIAL
-    Serial.print(F("X = 1 -> ")); Serial.println(X);
-    Serial.print(time->hours); Serial.print(':');
-    Serial.print(time->minutes); Serial.print(':');
-    Serial.print(time->seconds); Serial.println();
-#endif
+// #ifdef HAVE_SERIAL
+//     Serial.print(F("X = 1 -> ")); Serial.println(X);
+//     Serial.print(time->hours); Serial.print(':');
+//     Serial.print(time->minutes); Serial.print(':');
+//     Serial.print(time->seconds); Serial.println();
+// #endif
 }
 
 void displayDate(TimeStruct *time, byte offset, byte maxLen) {
@@ -141,34 +155,36 @@ void displayDate(TimeStruct *time, byte offset, byte maxLen) {
     // byte len = ledMatrix.stringWidth(str) - 1;
     // ledMatrix.drawString(offset + (maxLen-len)/2, str);
 
-#ifdef HAVE_SERIAL
-    Serial.print(time->dayOfWeek); Serial.print(',');
-    Serial.print(time->dayOfMonth); Serial.print('/');
-    Serial.print(time->month); Serial.print('/');
-    Serial.println(time->year);
-#endif
+// #ifdef HAVE_SERIAL
+//     Serial.print(time->dayOfWeek); Serial.print(',');
+//     Serial.print(time->dayOfMonth); Serial.print('/');
+//     Serial.print(time->month); Serial.print('/');
+//     Serial.println(time->year);
+// #endif
 }
 
 #define ST_DISPLAY         0
-#define ST_SET_YEAR        1
-#define ST_SET_MONTH       2
-#define ST_SET_DAY_OF_WEEK 3
-#define ST_SET_DAY         4
-#define ST_SET_HOUR        5
-#define ST_SET_MINUTES     6
-#define ST_SET_SECONDS     7
+#define ST_SET_OFFSET      1
+#define ST_SET_YEAR        2
+#define ST_SET_MONTH       3
+#define ST_SET_DAY_OF_WEEK 4
+#define ST_SET_DAY         5
+#define ST_SET_HOUR        6
+#define ST_SET_MINUTES     7
+#define ST_SET_SECONDS     8
 
-#define ST_LAST            7
+#define ST_LAST            8
 
 byte state = ST_DISPLAY;
 
-const char prompt1[] PROGMEM = "annee";
-const char prompt2[] PROGMEM = "mois";
-const char prompt3[] PROGMEM = "jour semaine";
-const char prompt4[] PROGMEM = "jour";
-const char prompt5[] PROGMEM = "heure";
-const char prompt6[] PROGMEM = "minute";
-const char prompt7[] PROGMEM = "seconde";
+const char prompt1[] PROGMEM = "offset";
+const char prompt2[] PROGMEM = "annee";
+const char prompt3[] PROGMEM = "mois";
+const char prompt4[] PROGMEM = "jour semaine";
+const char prompt5[] PROGMEM = "jour";
+const char prompt6[] PROGMEM = "heure";
+const char prompt7[] PROGMEM = "minute";
+const char prompt8[] PROGMEM = "seconde";
 
 const char * const prompts[] PROGMEM = {
     prompt1,
@@ -177,16 +193,20 @@ const char * const prompts[] PROGMEM = {
     prompt4,
     prompt5,
     prompt6,
-    prompt7
+    prompt7,
+    prompt8
 };
 
 byte button = 0;
 
-void updateDisplay() {
-    static TimeStruct time; // put static to avoid dynamic allocation of structure
+TimeStruct time; // put static to avoid dynamic allocation of structure
+
+void updateTimeFromRtc() {
     rtc.getTime(&time);
     toLocal(&time);
+}
 
+void updateDisplay() {
     ledMatrix.clear();
 
     if (state == ST_DISPLAY) {
@@ -201,7 +221,18 @@ void updateDisplay() {
         X = ledMatrix.drawChar(X, ' ');
 
         X = 1;
-        if (state == ST_SET_YEAR) {
+        if (state == ST_SET_OFFSET) {
+            byte offset = rtc.registerRead(DS3231_REG_Offset);
+            // registerRead returns a byte, we must convert it to signed value
+            if (offset >= 128) {
+                X = ledMatrix.drawChar(X, '~'); // '-' is not in reduced charmap, but '~' yes
+                offset = ~offset + 1;
+            }
+            X = ledMatrix.drawChar(X, '0' + offset / 100);
+            offset = offset % 100;
+            X = ledMatrix.drawChar(X, '0' + offset / 10);
+            X = ledMatrix.drawChar(X, '0' + offset % 10);
+        } else if (state == ST_SET_YEAR) {
             X = ledMatrix.drawChar(X, '2');
             X = ledMatrix.drawChar(X, '0');
             X = ledMatrix.drawChar(X, '0' + time.year / 10);
@@ -291,43 +322,39 @@ void handleState(short delta) {
         if (changeValue(&intensity, delta, 0, 7, 0)) {
             ledMatrix.setIntensity(intensity);
         }
+    } else if (state == ST_SET_OFFSET) {
+        value = rtc.registerRead(DS3231_REG_Offset);
+        if (changeValue(&value, delta, 0, 255)) {
+            rtc.registerWrite(DS3231_REG_Offset, value);
+        }
     } else if (state == ST_SET_YEAR) {
-        value = rtc.bcdToDec(rtc.registerRead(DS3231_REG_Year));
-        if (changeValue(&value, delta, 0, 99)) {
-            rtc.registerWrite(DS3231_REG_Year, rtc.decToBcd(value));
+        if (changeValue(&(time.year), delta, 0, 99)) {
+            rtc.registerWrite(DS3231_REG_Year, rtc.decToBcd(time.year));
         }
     } else if (state == ST_SET_MONTH) {
-        value = rtc.bcdToDec(rtc.registerRead(DS3231_REG_Month) & 0x7F);
-        if (changeValue(&value, delta, 1, 12)) {
-            rtc.registerWrite(DS3231_REG_Month, rtc.decToBcd(value));
+        if (changeValue(&(time.month), delta, 1, 12)) {
+            rtc.registerWrite(DS3231_REG_Month, rtc.decToBcd(time.month));
         }
     } else if (state == ST_SET_DAY_OF_WEEK) {
-        value = rtc.registerRead(DS3231_REG_Day);
-        if (changeValue(&value, delta, 1, 7)) {
-            rtc.registerWrite(DS3231_REG_Day, value);
+        if (changeValue(&(time.dayOfWeek), delta, 1, 7)) {
+            rtc.registerWrite(DS3231_REG_Day, time.dayOfWeek);
         }
     } else if (state == ST_SET_DAY) {
-        byte month = rtc.bcdToDec(rtc.registerRead(DS3231_REG_Month) & 0x1F);
-        byte year  = rtc.bcdToDec(rtc.registerRead(DS3231_REG_Year));
-        byte lastDay = lastDayOfMonth(month, year);
-        value = rtc.bcdToDec(rtc.registerRead(DS3231_REG_Date));
-        if (changeValue(&value, delta, 1, lastDay)) {
-            rtc.registerWrite(DS3231_REG_Date, rtc.decToBcd(value));
+        byte lastDay = lastDayOfMonth(time.month, time.year);
+        if (changeValue(&(time.dayOfMonth), delta, 1, lastDay)) {
+            rtc.registerWrite(DS3231_REG_Date, rtc.decToBcd(time.dayOfMonth));
         }
     } else if (state == ST_SET_HOUR) {
-        value = rtc.bcdToDec(rtc.registerRead(DS3231_REG_Hours) & 0x3F);
-        if (changeValue(&value, delta, 0, 23)) {
-            rtc.registerWrite(DS3231_REG_Hours, rtc.decToBcd(value));
+        if (changeValue(&(time.hours), delta, 0, 23)) {
+            rtc.registerWrite(DS3231_REG_Hours, rtc.decToBcd(time.hours));
         }
     } else if (state == ST_SET_MINUTES) {
-        value = rtc.bcdToDec(rtc.registerRead(DS3231_REG_Minutes));
-        if (changeValue(&value, delta, 0, 59)) {
-            rtc.registerWrite(DS3231_REG_Minutes, rtc.decToBcd(value));
+        if (changeValue(&(time.minutes), delta, 0, 59)) {
+            rtc.registerWrite(DS3231_REG_Minutes, rtc.decToBcd(time.minutes));
         }
     } else if (state == ST_SET_SECONDS) {
-        value = rtc.bcdToDec(rtc.registerRead(DS3231_REG_Seconds));
-        if (changeValue(&value, delta, 0, 59)) {
-            rtc.registerWrite(DS3231_REG_Seconds, rtc.decToBcd(value));
+        if (changeValue(&(time.seconds), delta, 0, 59)) {
+            rtc.registerWrite(DS3231_REG_Seconds, rtc.decToBcd(time.seconds));
         }
     }
 }
@@ -362,6 +389,8 @@ void handleButton(byte newButton) {
 }
 
 volatile bool clockTick = 0;
+volatile unsigned long clockCount = 0; // count ticks
+
 volatile bool buttonChange = 0;
 
 #ifdef USE_BUTTONS
@@ -374,9 +403,32 @@ ISR(PCINT0_vect) {
 }
 #endif
 
+#ifdef ARDUINO_TINY
+byte subTick=0;
+#endif
+
 ISR(TIMER1_COMPA_vect) {
-    clockTick = 1;
+#ifdef ARDUINO_TINY
+    if (subTick==125) {
+        subTick=0;
+        clockTick = 1;
+        clockCount++;
+    } else {
+        subTick++;
+    }
+#endif
 }
+
+#ifdef HAVE_SERIAL
+void printTime(TimeStruct *time) {
+    Serial.print(time->year);       Serial.print('/');
+    Serial.print(time->month);      Serial.print('/');
+    Serial.print(time->dayOfMonth); Serial.print(' ');
+    Serial.print(time->hours);      Serial.print(':');
+    Serial.print(time->minutes);    Serial.print(':');
+    Serial.println(time->seconds);
+}
+#endif
 
 void loop() {
 #ifdef DEBUG_BUTTONS
@@ -407,13 +459,13 @@ void loop() {
         while(TCNT1 < timer1); // wait 1562 ticks = 100ms
 #else // ARDUINO_TINY
         byte timer1 = TCNT1;
-        if(timer1 > 61-6) {
-            byte timerLoop = timer1 - (61-6);
+        if(timer1 > 125-12) {
+            byte timerLoop = timer1 - (125-12);
             while(TCNT1 >= timer1 || TCNT1 < timerLoop); // wait for counter loop
         } else {
-            timer1 += 6;
+            timer1 += 12;
         } 
-        while(TCNT1 < timer1); // wait 6 ticks = ~100ms
+        while(TCNT1 < timer1); // wait 12 ticks = ~100ms
 #endif
 #ifdef USE_BUTTONS
         byte newButton = buttons.read();
@@ -425,6 +477,24 @@ void loop() {
     }
     if (clockTick) {
         clockTick = 0;
+//         if(clockCount >= 1200) {
+// #ifdef HAVE_SERIAL
+//             Serial.print("BEFORE : ");
+//             printTime(&time);
+// #endif
+//             // read RTC time every 20 minutes (correct 1MHz tiny shifting 1s/30min)
+//             updateTimeFromRtc();
+// #ifdef HAVE_SERIAL
+//             Serial.print("AFTER : ");
+//             printTime(&time);
+// #endif
+//             clockCount=0;
+//         } else {
+            incrementSecond(&time);
+#ifdef HAVE_SERIAL
+            printTime(&time);
+#endif
+        // }
         needUpdate = 1;
     }
     if (needUpdate) {
